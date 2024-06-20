8be243a44babd7b6b0409041cad00ecf8becb6bc
정호연 <jea7730@gmail.com>
2024-06-20 12:52:14 +0900

    insert 사용하여 names, age 변수 값 넣기

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 // console.dir(sqlite3);
 
 const db = new sqlite3.Database('databasefile.db');
-console.log(db);
+// console.log(db);
 
 // database 테이블 쿼리 만들기
 const createTableQuery = `CREATE TABLE IF NOT EXISTS member (
@@ -15,7 +15,14 @@ const createTableQuery = `CREATE TABLE IF NOT EXISTS member (
 let names = '정호연';
 let age = 27;
 
-db.run(createTableQuery, (err) => {
+let test = {
+  name: ['이연승', '정호연'],
+  age: [30, 27],
+};
+
+let insertQurey = `INSERT INTO member (name, age) VALUES (?, ?)`;
+
+db.run(insertQurey, [test.name, test.age], (err) => {
   if (err) {
     console.log(err.message);
   } else {
