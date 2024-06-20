a6460a375fa569f1b806f77f2670a7b52d709be4
정호연 <jea7730@gmail.com>
2024-06-20 12:13:50 +0900

    생성할 데이터베이스에 들어갈 테이블 쿼리 만들기

--- test.js
+++ test.js
@@ -2,4 +2,12 @@ const sqlite3 = require('sqlite3').verbose();
 // console.dir(sqlite3);
 
 const db = new sqlite3.Database('databasefile.db');
-console.log(db);
+// console.log(db);
+
+// database 테이블 쿼리 만들기
+const createTableQuery = `CREATE TABLE IF NOT EXISTS member (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL
+);
+`;
