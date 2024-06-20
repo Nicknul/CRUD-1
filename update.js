765cc6910f8b2b167465a6c34bf99df4165092d2
정호연 <jea7730@gmail.com>
2024-06-20 20:21:20 +0900

    update 확인 완료

--- update.js
+++ update.js
@@ -1,4 +1,13 @@
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('./databasefile.db');
+const db = new sqlite3.Database('databasefile.db');
 
 const updateData = db.prepare(`UPDATE member SET name = ? WHERE id = ?`);
+
+updateData.run('이연승', 1, (err) => {
+  if (err) {
+    console.error('데이터 업데이트 중 오류 발생:', err.message);
+  } else {
+    console.log(`ID 1 사용자의 이름이 이연승으로 업데이트되었습니다.`);
+  }
+  db.close();
+});
