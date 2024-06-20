d7ad7d97076a399eea80f9be228ac36250b0f3d4
정호연 <jea7730@gmail.com>
2024-06-20 21:15:27 +0900

    DELETE 기능 확인 완료

--- delete.js
+++ delete.js
@@ -3,3 +3,16 @@ const db = new sqlite3.Database('databasefile.db');
 
 const deleteMemberQurey = `DELETE FROM member WHERE id = ?`;
 const userId = 4;
+
+db.run(deleteMemberQurey, userId, (err) => {
+  if (err) {
+    console.error(`데이터 삭제 중 오류 발생:`, err.message);
+  } else {
+    console.log(`ID ${userId}가 삭제되었습니다.`);
+  }
+  db.close((closeErr) => {
+    if (closeErr) {
+      console.error(`데이터베이스 닫기 중 오류 발생:`, closeErr.message);
+    }
+  });
+});
